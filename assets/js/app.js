e9e1cf19f99727dfcef5ee5774f5cb6f757c81dc
Stas Schegolev <stanisla777@mail.ru>
2017-09-11 03:32:18 +0400

    ADd

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 
 $(document).ready(function ($) {
 
+
+
+/*Слайдер главного экрана*/
     $('.type-job-slider').slick({
         slidesToShow: 1,
         dots: true,
@@ -15,10 +18,40 @@ $(document).ready(function ($) {
         arrows: false
     });
 
-    //var header = $('header');
-    //$(window).on('scroll', function(e){
-    //    header.css('top', $(this).scrollTop());
-    //});
+    /*Меню*/
+    $('.menu-icon').on("click",function(e){
+
+        $('.media-menu').css("display","block");
+        e.preventDefault();
+    });
+    $(".icon-close").click(function(){
+        $('.media-menu').css("display","none");
+    });
+    $(".main-wrap").click(function(){
+        $('.media-menu').css("display","none");
+    });
+
+
+
+
+    $('.menu-icon').on("click",function(e){
+
+        $('.media-menu').css("display","block");
+        e.preventDefault();
+    });
+    $(".icon-close").click(function(){
+        $('.media-menu').css("display","none");
+    });
+    $(".main-wrap").click(function(){
+        $('.media-menu').css("display","none");
+    });
+
+
+    /*Прижатый хедер*/
+    var header = $('header');
+    $(window).on('scroll', function(e){
+        header.css('top', $(this).scrollTop());
+    });
 
     $(".tabs_menu li").click(function () {
         if (!$(this).hasClass("active")) {
@@ -30,14 +63,9 @@ $(document).ready(function ($) {
         }
     });
 
-    //$(document).ready( function() {
-    //    $(".file-upload input[type=file]").change(function(){
-    //        var filename = $(this).val().replace(/.*\\/, "");
-    //        $("#filename").val(filename);
-    //    });
-    //});
 
 
+    /*Функция для добавления картинки пользователем*/
     function readURL(input) {
 
         if (input.files && input.files[0]) {
@@ -56,6 +84,59 @@ $(document).ready(function ($) {
     $("#imgInput").change(function(){
         readURL(this);
     });
+
+    /*Смена экранов регистрации*/
+
+    var steps = $("form").children(".step"); // находим все шаги формы
+    $(steps[0]).show(); // показываем первый шаг
+    var current_step = 0; // задаем текущий шаг
+    $("a.next").click(function(){	// Событие клика на ссылку "Следующий шаг"
+        if (current_step == steps.length-2) { // проверяем, будет ли следующий шаг последним
+            $(this).hide(); // скрываем ссылку "Следующий шаг"
+            $("form input[type=submit]").show(); // показываем кнопку "Регистрация"
+        }
+        $("a.back").show(); // показываем ссылку "Назад"
+        current_step++; // увеличиваем счетчик текущего слайда
+        changeStep(current_step); // меняем шаг
+    });
+
+    $("a.back").click(function(){	// Событие клика на маленькое изображение
+        if (current_step == 1) { // проверяем, будет ли предыдущий шаг первым
+            $(this).hide(); // скрываем ссылку "Назад"
+        }
+        $("form input[type=submit]").hide(); // скрываем кнопку "Регистрация"
+        $("a.next").show(); // показываем ссылку "Следующий шаг"
+        current_step--; // уменьшаем счетчик текущего слайда
+        changeStep(current_step);// меняем шаг
+    });
+
+    function changeStep(i) { // функция смены шага
+        $(steps).hide(); // скрываем все шаги
+        $(steps[i]).show(); // показываем текущий
+    }
+
+
+    for ( i = 1980; i < 2020; i++) {
+        $('<option value='+i+'>'+i+'</option>').appendTo('select#year-issue')
+    }
+
+    /*Календарь*/
+    $('.datetimepicker').datetimepicker({
+        //datepicker:false
+        format:'d.m.Y',
+        timepicker:false,
+        lang:'ru'
+    });
+    $('.datetimepicker-year').datetimepicker({
+        format:'Y',
+        //datepicker:false,
+        timepicker:false,
+        lang:'ru',
+        closeOnDateSelect:true,
+        closeOnWithoutClick :true
+    });
+
+
 
 
 
