80e0f75cd3fff62e623ba40bed539d12e6046461
Stas Schegolev <stanisla777@mail.ru>
2017-09-14 06:45:34 +0400

    Add

--- assets/js/app.js
+++ assets/js/app.js
@@ -257,45 +257,45 @@ $(document).ready(function ($) {
 
 
     /*Яндекс карты*/
-    //ymaps.ready(init);
-    //
-    //function init () {
-    //    var myMap = new ymaps.Map("map", {
-    //        // Центр карты, указываем коордианты
-    //        center:[55.752161956105276,37.61949517968746],
-    //        // Масштаб, тут все просто
-    //        zoom: 16,
-    //        // Отключаем все элементы управления
-    //        controls: []
-    //    });
-    //
-    //    var myGeoObjects = [];
-    //
-    //    // Наша метка, указываем коордианты
-    //    myGeoObjects = new ymaps.Placemark([55.800151390638646,37.61400201562497],{
-    //        balloonContentBody: 'Текст в балуне'
-    //    },{
-    //        iconLayout: 'default#image',
-    //        // Путь до нашей картинки
-    //        iconImageHref: 'assets/img/icon-tel.png',
-    //        // Размер по ширине и высоте
-    //        iconImageSize: [100, 100],
-    //        // Смещение левого верхнего угла иконки относительно
-    //        // её «ножки» (точки привязки).
-    //        iconImageOffset: [-35, -35]
-    //    });
-    //
-    //    var clusterer = new ymaps.Clusterer({
-    //        clusterDisableClickZoom: false,
-    //        clusterOpenBalloonOnClick: false
-    //    });
-    //
-    //    clusterer.add(myGeoObjects);
-    //    myMap.geoObjects.add(clusterer);
-    //    // Отлючаем возможность изменения масштаба
-    //    myMap.behaviors.disable('scrollZoom');
-    //
-    //}
+    ymaps.ready(init);
+
+    function init () {
+        var myMap = new ymaps.Map("map", {
+            // Центр карты, указываем коордианты
+            center:[55.752161956105276,37.61949517968746],
+            // Масштаб, тут все просто
+            zoom: 16,
+            // Отключаем все элементы управления
+            controls: []
+        });
+
+        var myGeoObjects = [];
+
+        // Наша метка, указываем коордианты
+        myGeoObjects = new ymaps.Placemark([55.800151390638646,37.61400201562497],{
+            balloonContentBody: 'Текст в балуне'
+        },{
+            iconLayout: 'default#image',
+            // Путь до нашей картинки
+            iconImageHref: 'assets/img/icon-tel.png',
+            // Размер по ширине и высоте
+            iconImageSize: [100, 100],
+            // Смещение левого верхнего угла иконки относительно
+            // её «ножки» (точки привязки).
+            iconImageOffset: [-35, -35]
+        });
+
+        var clusterer = new ymaps.Clusterer({
+            clusterDisableClickZoom: false,
+            clusterOpenBalloonOnClick: false
+        });
+
+        clusterer.add(myGeoObjects);
+        myMap.geoObjects.add(clusterer);
+        // Отлючаем возможность изменения масштаба
+        myMap.behaviors.disable('scrollZoom');
+
+    }
 
 
     /*Доработать, вычисляет не каждый хедер*/
