0a931c30b504160627ed20b991d0f95729781ea9
Stas Schegolev <stanisla777@mail.ru>
2017-11-05 22:21:05 +0300

    Add

--- js/tabs.js
+++ js/tabs.js
@@ -10,14 +10,13 @@ $(document).ready(function(){
 		$("#"+tab_id).addClass('current');
     });
 
-    $('.button-next').click(function(){
+    $('.button-next').on('click',function(){
         var tab_id = $(this).attr('data-tab');
+        $(this).parents('.tab-content').removeClass('current');
+        $(this).parents('.tab-content').next('.tab-content').addClass('current');
+        $('.acc-setting-tab li').removeClass('current');
 
-        $('ul.tabs li').removeClass('current');
-        $('.tab-content').removeClass('current');
-
-        $(this).addClass('current');
-        $("#"+tab_id).addClass('current');
+        $(this).parents('form').siblings('.acc-setting-tab').find('li[data-tab='+tab_id+']').next('li').addClass('current');
     })
 
 });
