abee5d6eaca4136b02716830b0144eb770443568
Stas Schegolev <stanisla777@mail.ru>
2018-04-19 22:48:39 +0400

    Маленькие доработки

--- Piroll/assets/js/app.js
+++ Piroll/assets/js/app.js
@@ -14,28 +14,45 @@ $(document).ready(function ($) {
     });
 
 
+    var he = $(".wrapper-foto-interviewee").height();
+
+    //$(window).scroll(function(){
+    //    if ( $(window).scrollTop() >= $(".wrapper-foto-interviewee").offset().top)  {
+    //        $(".wrapper-foto-interviewee").hide()
+    //
+    //    }
+    //});
+
+
 
     var windowHeight = $(window).height();
 
-    function Animat (obj){
-        this.elem = obj.elem;
-        this.anim_class = obj.anim_class;
-        var that = this;
-        this.method = function(){
+function Animat (obj){
+    this.elem = obj.elem;
+    this.anim_class = obj.anim_class;
+    var that = this;
+    this.method = function(){
 
-            $(document).on('scroll', function() {
-                $(that.elem).each(function() {
-                    var self = $(this);
-                    height = self.offset().top + self.height();
-                    if ($(document).scrollTop() + windowHeight >= height) {
-                        $(self).addClass(that.anim_class);
-                    }
-                });
+        $(document).on('scroll', function() {
+            $(that.elem).each(function() {
+                var self = $(this);
+                height = self.offset().top + self.height();
+                height_2 = self.height();
+                if ($(document).scrollTop() + windowHeight >= height) {
+                    $(self).addClass(that.anim_class);
+                }
+
+                if ( $(window).scrollTop() >=$(self).offset().top+height_2||$(document).scrollTop() + windowHeight+height_2<= height)  {
+                    $(self).removeClass(that.anim_class)
+
+                }
+
             });
+        });
 
-        };
-        that.method();//вызываю функцию внутри класса, чтобы сразу её и выполнять
-    }
+    };
+    that.method();//вызываю функцию внутри класса, чтобы сразу её и выполнять
+}
 
 
 
@@ -47,6 +64,15 @@ $(document).ready(function ($) {
         elem:".wrapper-another-advantage",
         anim_class:'zoomIn'
     });
+
+    new Animat({
+        elem:".wrapper-interview",
+        anim_class:'box'
+    });
+    new Animat({
+            elem:".wrapper-foto-interviewee",
+            anim_class:'zoomIn'
+        });
 
 
 
