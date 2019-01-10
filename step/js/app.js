95bd7f9ac222fe0bd321c404bfb0929128521192
Станислав <stanisla777@mail.ru>
2019-01-10 23:22:03 +0300

    Новый проект

--- step/js/app.js
+++ step/js/app.js
@@ -2,7 +2,23 @@
 
 $(document).ready(function ($) {
 
+//Добавить-------------------------
 
+    //Медиа главного меню
+
+    $('.main-site-bar__icon').on('click',function () {
+
+        if($(this).siblings('.main-site-bar__list').hasClass('main-site-bar__list-active')){
+            $(this).find('.close-burger').css('border-bottom','3px solid #fff').removeClass('close-cross');
+            $(this).siblings('.main-site-bar__list').css('display','none').removeClass('main-site-bar__list-active');
+        }
+        else {
+            $(this).find('.close-burger').css('border','0').addClass('close-cross');
+            $(this).siblings('.main-site-bar__list').css('display','flex').addClass('main-site-bar__list-active');
+        }
+
+
+    })
 
 
 });
