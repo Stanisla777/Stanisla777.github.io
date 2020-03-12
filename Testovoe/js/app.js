794cb6344b2446a5be9d2ba309ef5841c3f23607
Станислав <stanisla777@mail.ru>
2020-03-12 18:34:19 +0300

    Правки

---
+++ Testovoe/js/app.js
@@ -0,0 +1,20 @@
+
+
+$(document).ready(function ($) {
+    $('.wrapper-slider-reviews .owl-carousel').owlCarousel({
+        items: 1,
+        loop: false,
+        // center: true,
+        nav: true,
+        navText: ['', ''],
+        mouseDrag: true,
+        touchDrag: true,
+        onInitialized: function () {
+
+        }
+    });
+
+
+});
+
+
