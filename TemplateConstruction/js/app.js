6bf4fe0b639f0421386d700c5f2173b0276417cc
Станислав <stanisla777@mail.ru>
2021-02-19 14:39:02 +0300

    Правки

--- TemplateConstruction/js/app.js
+++ TemplateConstruction/js/app.js
@@ -1,5 +1,4 @@
 "use strict";
-
 $(document).ready(function () {
   $('.accordion-title').on('click', function () {
     if ($(this).hasClass('active')) {
@@ -7,35 +6,32 @@ $(document).ready(function () {
     } else {
       $(this).addClass('active');
     }
-
     $(this).siblings('.accordion-content').slideToggle(500, 'swing');
-  }); //  Модалка
-
+  });
   var page_overlay = '<div id="lean_overlay"></div>'; //фон затемнение
-
   $('.js-modal').on('click', function () {
     //Открываю модально окно по клику
     if ($('body').find('#lean_overlay').length === 0) {
       $('body').append(page_overlay);
     }
-
     var perem_modal = $(this).attr('data-modal');
     $('.modal[data-modal=' + perem_modal + ']').css('display', 'flex');
-    $('#lean_overlay').show(); //фон затемнение
+    $('#lean_overlay').show();
+    OffScroll();
   });
   $('.close-modal, .modal__form button').on('click', function () {
     //Закрываю модальное окно по клику на крестик
     $('.modal').css('display', 'none');
     $('#lean_overlay').hide();
+    $(window).unbind('scroll');
   });
   $('.modal').on('click', function (e) {
-    //Закрываю модальное окно по клику рабочей области
     $('.modal').css('display', 'none');
     $("#lean_overlay").hide();
+    $(window).unbind('scroll');
   }).on('click', '.modal-container-js', function (e) {
-    //Запрещаю закрывать окно по клику на само молдальное окно
     e.stopPropagation();
-  }); //Когда окно повится можно добавить функцию запрета скроллинга страницы
+  });
 
   function OffScroll() {
     var winScrollTop = $(window).scrollTop();
