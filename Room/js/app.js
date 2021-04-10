316223d2e31b199865781da9eaa34e7f8c4e9166
Станислав <stanisla777@mail.ru>
2021-04-10 03:16:26 +0300

    Правки

---
+++ Room/js/app.js
@@ -0,0 +1,27 @@
+"use strict";
+
+window.onload = function () {
+  var page_overlay = document.querySelector("#lean_overlay");
+  var modal = document.querySelector('.modal');
+  var but = document.querySelectorAll('.js-modal');
+
+  for (var el = 0; el < but.length; el++) {
+    but[el].onclick = function () {
+      page_overlay.setAttribute('style', 'display:block');
+      modal.setAttribute('style', 'display:flex');
+    };
+  }
+
+  document.querySelector('.close-modal').onclick = function () {
+    page_overlay.setAttribute('style', 'display:none');
+    modal.setAttribute('style', 'display:none');
+  };
+
+  var price = document.querySelectorAll('.price');
+
+  for (var item = 0; item < price.length; item++) {
+    var val = price[item].textContent;
+    var new_price = val.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ');
+    price[item].textContent = new_price;
+  }
+};
